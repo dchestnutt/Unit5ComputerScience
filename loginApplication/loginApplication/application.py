b4bae3a9aa86a8b1aa1cc744173532455ffec092
Daniel Chestnutt <dchestnutt4@gmail.com>
2018-10-19 10:06:58 +0100

    19/10 DC AM

--- loginApplication/loginApplication/application.py
+++ loginApplication/loginApplication/application.py
@@ -9,46 +9,153 @@ window.title("Example Code")
 
 usernameNew = StringVar()
 passwordNew = StringVar()
+canViewPatientData = StringVar()
+canEditPatientData = StringVar()
+canViewConsultations = StringVar()
+canEditConsultations = StringVar()
+canCommunicatePatients = StringVar()
+canViewStatistics = StringVar()
+canViewAppointments = StringVar()
+canCancelAppointments = StringVar()
+canCreateUsers = StringVar()
+canEditUsers = StringVar()
+titleNew = StringVar()
+firstnameNew = StringVar()
+surnameNew = StringVar()
+emailNew = StringVar()
 
 # ***** Defining Functions ***** #
 
 # Create a New User Account #
 def createUser():
     tempWindow = Tk()
-    tempWindow.geometry('500x300+100+100')
+    tempWindow.geometry('700x400+100+100')
     tempWindow.title("Create a New User")
 
-    #labelInstructions = Label(tempWindow, 
-    #                         text="Please enter new user details below.", 
-    #                         width=20, 
-    #                         font=("Century Gothic", 10),
-    #                         anchor=CENTER)                 
-    #labelInstructions.pack(side=LEFT, padx=5, pady=5)
-    #labelInstructions.grid(row=0, column=0, columnspan=3) 
+    labelInstructions = Label(tempWindow, 
+                             text="Antrim Castle Surgery - Administration Portal (Create a User)",
+                             font=("corbel bold", 10),
+                             anchor=N)                 
+    labelInstructions.grid(row=0, column=1, columnspan=8) 
 
-    #spacerLabel = Label(tempWindow, text=" ", width=20)
-    #spacerLabel.pack(side=LEFT, padx=5, pady=5)
-    #spacerLabel.grid(row=1, column=0, columnspan=3)
+    spacerLabel = Label(tempWindow, text=" ", width=20)
+    spacerLabel.grid(row=1, column=0, columnspan=8)
 
     labelUsername = Label(tempWindow, 
                           text="Username: ", 
-                          width=4, 
-                          font=("Century Gothic", "bold", 10))                 
-    labelUsername.pack(side=LEFT, padx=5, pady=5)
-    #labelUsername.grid(row=2, column=0)                   
+                          font=("corbel", 10))                 
+    labelUsername.grid(row=2, column=0)                   
     entryUsername = Entry(tempWindow, textvar=usernameNew) 
-    entryUsername.pack(fill=X, padx=5, expand=True)
-    #entryUsername.grid(row=2, column=1, columnspan=2)            
+    entryUsername.grid(row=2, column=1, columnspan=2)            
     
     labelPassword = Label(tempWindow, 
-                          text="Username: ", 
-                          width=4, 
-                          font=("Century Gothic", "bold", 10))                 
-    labelPassword.pack(side=LEFT, padx=5, pady=5)
-    #labelPassword.grid(row=3, column=0)                   
+                          text="Password: ", 
+                          font=("corbel", 10))                 
+    labelPassword.grid(row=3, column=0)                   
     entryPassword = Entry(tempWindow, textvar=passwordNew) 
-    entryPassword.pack(fill=X, padx=5, expand=True)
-    #entryPassword.grid(row=3, column=1, columnspan=2)
+    entryPassword.grid(row=3, column=1, columnspan=2)
+
+    spacerLabel = Label(tempWindow, text=" ", width=20)
+    spacerLabel.grid(row=4, column=0, columnspan=3)
+    
+    labelFirstName = Label(tempWindow, 
+                          text="Forename: ", 
+                          font=("corbel", 10))                 
+    labelFirstName.grid(row=5, column=0)                   
+    entryFirstName = Entry(tempWindow, textvar=firstnameNew) 
+    entryFirstName.grid(row=5, column=1, columnspan=2)            
+    
+    labelSurname = Label(tempWindow, 
+                          text="Surname: ", 
+                          font=("corbel", 10))                 
+    labelSurname.grid(row=6, column=0)                   
+    entrySurname = Entry(tempWindow, textvar=surnameNew) 
+    entrySurname.grid(row=6, column=1, columnspan=2)
+
+    labelTitle = Label(tempWindow, 
+                          text="Title: ", 
+                          font=("corbel", 10))                 
+    labelTitle.grid(row=7, column=0)                   
+    entryTitle = Entry(tempWindow, textvar=titleNew) 
+    entryTitle.grid(row=7, column=1, columnspan=2)
+
+    spacerLabel = Label(tempWindow, text=" ")
+    spacerLabel.grid(row=0, column=4, rowspan=10)
+
+    spacerLabel = Label(tempWindow, text=" ", width=20)
+    spacerLabel.grid(row=8, column=0, columnspan=8)
+
+    labelEmail = Label(tempWindow,
+                       text="E-mail: ",
+                       font=("corbel", 10))
+    labelEmail.grid(row=9, column=0)
+    entryEmail = Entry(tempWindow, textvar=emailNew)
+    entryEmail.grid(row=9, column=1, columnspan=4)
+
+    labelPermissions = Label(tempWindow,    
+                             text="User Permissions: ",  
+                             font=("corbel", 10))
+    labelPermissions.grid(row=2, column=5, columnspan=2)                                                    
+    Checkbutton(tempWindow, 
+                text="View Patient Data", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canViewPatientData,
+                onvalue='Yes', offvalue='No').grid(row=2, column=7, sticky='W',)  
+    Checkbutton(tempWindow, 
+                text="Edit Patient Data", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canEditPatientData,
+                onvalue='Yes', offvalue='No').grid(row=3, column=7, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="View Patient Consultation Notes", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canViewConsultations,
+                onvalue='Yes', offvalue='No').grid(row=4, column=7, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="Edit Patient Consultation Notes", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canEditConsultations,
+                onvalue='Yes', offvalue='No').grid(row=5, column=7, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="Communicate with Patients", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canCommunicatePatients,
+                onvalue='Yes', offvalue='No').grid(row=6, column=7, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="View Patient Statistics", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canViewStatistics,
+                onvalue='Yes', offvalue='No').grid(row=2, column=8, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="View Patient Appointments", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canViewAppointments,
+                onvalue='Yes', offvalue='No').grid(row=3, column=8, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="Cancel Patient Appointments", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canCancelAppointments,
+                onvalue='Yes', offvalue='No').grid(row=4, column=8, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="Create New User Accounts", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canCreateUsers,
+                onvalue='Yes', offvalue='No').grid(row=5, column=8, sticky='W',)
+    Checkbutton(tempWindow, 
+                text="Edit User Accounts", 
+                cursor='hand2',
+                font=("corbel", 10),
+                variable=canEditUsers,
+                onvalue='Yes', offvalue='No').grid(row=6, column=8, sticky='W',)
 
 # Edit an Existing User Account #
 def editUser():
